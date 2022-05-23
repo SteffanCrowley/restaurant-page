cbd48b49687c74d4616256a2de5d06c2f1a96e57
SteffanCrowley <steffancrow@gmail.com>
2022-05-23 13:47:13 -0700

    Import a function from module into index.js

---
+++ src/modules/contact.js
@@ -0,0 +1,9 @@
+function component() {
+  const element = document.createElement("div");
+  element.innerHTML = "contact";
+  element.classList.add("hello");
+
+  return element;
+}
+
+export { component };
